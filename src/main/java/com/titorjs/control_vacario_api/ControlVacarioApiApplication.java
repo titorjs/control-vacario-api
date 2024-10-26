9152d727db17a9b5dfdfed6c27d4a5e00bf7e9f3
Tito Jaramillo S <titoj740@gmail.com>
2024-10-26 11:55:10 -0500

    Initial commit

---
+++ src/main/java/com/titorjs/control_vacario_api/ControlVacarioApiApplication.java
@@ -0,0 +1,13 @@
+package com.titorjs.control_vacario_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlVacarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlVacarioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/titorjs/control_vacario_api/ControlVacarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titorjs.control_vacario_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlVacarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
